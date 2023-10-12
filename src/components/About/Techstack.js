0c3e06bd7a8d740e65ac93c58e97cbcfc46203a0
Aditya Jain <jain.aditya1907@gmail.com>
2023-10-12 14:13:30 +0530

    second update

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -5,59 +5,50 @@ import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
   DiPython,
   DiGit,
   DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
+  SiNextdotjs
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
+import {BsFiletypeHtml} from "react-icons/bs" ;
+import {AiOutlineHtml5} from "react-icons/ai" ;
+import {
+  DiBootstrap,
+  DiCode,
+  DiJsBadge
+} from "react-icons/di" ;
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
+        <DiCode />
+      </Col><Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
+        <DiJsBadge />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
+      <Col xs={4} md={2} className="tech-icons">
+        <BsFiletypeHtml />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col> */}
+        <AiOutlineHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
